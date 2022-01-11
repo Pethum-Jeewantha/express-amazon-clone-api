d20e49a12d3953bc2fbb6ccc7d8ec9601a3084f9
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2022-01-11 14:48:29 +0530

    implement user save & authentication

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    userId: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+});
+
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).matches(/[A-Za-z ]+/).required(),
+        userId: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(1024).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
