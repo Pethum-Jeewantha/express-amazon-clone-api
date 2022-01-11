b30a0bc9de80e6515dd15bda9fb993ea461c6bd1
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2022-01-11 15:50:51 +0530

    add login

---
+++ routes/login.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const {User} = require('../models/user');
+const express = require('express');
+require('mongoose');
+const _ = require("lodash");
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({userId: req.body.userId});
+    if (!user) return res.status(400).send('Invalid email or password');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password');
+
+    res.header('x-auth-token', user.generateAuthToken()).send(_.pick(user, ['name', 'userId']));
+});
+
+function validate(req) {
+    const schema = {
+        userId: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(1024).required()
+    };
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
