8b8f8e9f48e8a8ecde5e49af496838fd1e663519
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2022-01-11 10:21:38 +0530

    implement get Item by id & validateObjectId

--- routes/items.js
+++ routes/items.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const Item = require('../models/item');
+const validateObjectId = require('../middleware/validateObjectId');
 
 router.get('/', async (req, res) => {
     res.send(await Item.find().sort('_id'));
+});
+
+router.get('/:id', validateObjectId, async (req, res) => {
+    const item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).send('The item with the given ID is not exists');
+
+    res.send(item);
 });
 
 module.exports = router;
