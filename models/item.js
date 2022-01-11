0b8e23fa63667b97e34573fa8ba79881431c4d21
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2022-01-11 10:07:39 +0530

    implement getAll Items

--- models/item.js
+++ models/item.js
@@ -1,29 +1,19 @@
 const mongoose = require('mongoose');
 
-const Rating = {
-    FIRST: 1,
-    SECOND: 2,
-    THIRD: 3,
-    FOURTH: 4,
-    FIFTH: 5
-}
-
 module.exports = mongoose.model('Item', new mongoose.Schema({
-    code: {
-        type: String,
-        required: true
-    },
     title: {
         type: String,
-        required: true
+        required: true,
     },
     image: {
         type: String,
-        required: true
+        required: true,
     },
     rating: {
-        type: Rating,
-        required: true
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
     },
     qty: {
         type: Number,
--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const Item = require('../models/item');
+
+router.get('/', async (req, res) => {
+    res.send(await Item.find().sort('_id'));
+});
+
 module.exports = router;
